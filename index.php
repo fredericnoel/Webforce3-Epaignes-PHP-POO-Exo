46226f06b3ae25349f8d0ce60c5d3888e8bea0ef
Frédéric NOËL <contact@fredericnoel.com>
2020-03-17 15:02:49 +0100

    Correction fonction de définition du fuseau horaire. Ajout tableau associatif et vérification.

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-date_timezone_set('Europe/Paris');
+date_default_timezone_set('Europe/Paris');
 require_once './functions/classAutoLoader.php';
 spl_autoload_register('classAutoloader');
+
+$valeurs = array(
+    'nom' => 'Michel',
+    'prenom' => 'Jean-Pierre',
+    'email' => 'jp@michel.com',
+    'password' => 'michel a la plage'
+);
+
+var_dump($valeurs);
