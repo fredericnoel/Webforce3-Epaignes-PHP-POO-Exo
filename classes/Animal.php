053524a1df8b0144b5094478a97ab1b458a9274c
Frédéric NOËL <contact@fredericnoel.com>
2020-03-17 12:17:36 +0100

    Classe Animal

---
+++ classes/Animal.php
@@ -0,0 +1,18 @@
+<?php
+
+abstract class Animal
+{
+    protected string $regne = 'animal';
+    protected float $taille;
+    protected float $masse;
+    protected string $typeRespiration;
+    protected float $consommationO2;
+    protected float $rejetCO2;
+
+    protected function respirer(string $typeR, float $conso, float $rejet)
+    {
+        $this->typeRespiration = $typeR;
+        $this->consommationO2 = $conso;
+        $this->rejetCO2 = $rejet;
+    }
+}
