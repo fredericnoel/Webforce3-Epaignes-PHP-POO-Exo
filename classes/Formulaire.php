7565811dd35c835b72f49c6cabacf6ad2f500891
Frédéric NOËL <contact@fredericnoel.com>
2020-03-19 10:33:42 +0100

    Affectation des paramètres aux propriétés de l'objet

--- classes/Formulaire.php
+++ classes/Formulaire.php
@@ -9,11 +9,24 @@ abstract class Formulaire
     protected $id;
     protected $class;
     protected $enctype;
+    protected $data;
 
-    public function __construct(string $url, string $fichier, string $idFormulaire, string $classFormulaire)
+    public function __construct(string $url, string $fichier, string $idFormulaire, string $classFormulaire, bool $modeEnvoiFrm = false)
     {
+        // Récupération du paramètre $url pour attribut action
+        $this->action = $url;
+
+        // Paramètre indiquant quel fichier de conf utiliser
         $path = './conf/' . $fichier . '.ini';
-        $fileData = parse_ini_file($path, true);
-        var_dump($fileData);
+        $this->data = parse_ini_file($path, true);
+
+        // id du formulaire
+        $this->id = $idFormulaire;
+
+        // classe CSS du formulaire
+        $this->class = $classFormulaire;
+
+        // dans le cas où le formulaire permettrait l'envoi de fichier
+        $this->enctype = $modeEnvoiFrm;
     }
 }
