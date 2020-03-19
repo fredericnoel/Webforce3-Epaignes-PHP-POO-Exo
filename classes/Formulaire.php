b6d00104e6343e3091a91793d67148c233df469a
Frédéric NOËL <contact@fredericnoel.com>
2020-03-19 10:18:40 +0100

    Ajout de paramètres au constructeur : - $url (pour destination du formulaire) - $fichier (fichier de conf) - $idFormulaire (identifiant balise form) - $classFormulaire (classe balise form)

--- classes/Formulaire.php
+++ classes/Formulaire.php
@@ -5,13 +5,15 @@ abstract class Formulaire
 {
     protected $action;
     protected $method = 'post';
+    protected $uploadFile = false;
     protected $id;
     protected $class;
     protected $enctype;
 
-    public function __construct(string $fichier)
+    public function __construct(string $url, string $fichier, string $idFormulaire, string $classFormulaire)
     {
         $path = './conf/' . $fichier . '.ini';
-        $fileData = parse_ini_file($path);
+        $fileData = parse_ini_file($path, true);
+        var_dump($fileData);
     }
 }
