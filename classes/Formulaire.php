656d0ab37d56d5cece15642ce5fe306d07bd532f
Frédéric NOËL <contact@fredericnoel.com>
2020-03-19 09:04:21 +0100

    Constructeur pour test lecture fichier .ini

--- classes/Formulaire.php
+++ classes/Formulaire.php
@@ -9,5 +9,9 @@ abstract class Formulaire
     protected $class;
     protected $enctype;
 
-
+    public function __construct(string $fichier)
+    {
+        $path = './conf/' . $fichier . '.ini';
+        $fileData = parse_ini_file($path);
+    }
 }
